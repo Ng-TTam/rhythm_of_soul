3d022e67009596f81fa282d84798a392fc94030c
ngthanhtam <you@example.com>
2025-04-12 09:27:16 +0700

    user home dashboard add

---
+++ frontend/rhythm_of_soul/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import Error404 from './pages/errors/Error404';
+// import Error500 from './pages/errors/error500';
+import Maintenance from './pages/errors/Maintenance';
+import Dashboard from './pages/dashboard/Dashboard';
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
